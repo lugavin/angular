6f51dd8acf7a7afebc95091b22ac7ca91291658e
Gavin <lugavin@outlook.com>
2017-09-02 20:18:56 +0800

    Update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,32 +1,42 @@
-/**
- * @namespace Base
- */
-(function (window) {
+(function (root, factory) {
+
+    if (typeof(Namespace) === 'undefined') {
+        Namespace = {};
+    }
 
     /**
-     * 声明类(JS中可以用一个函数或一个对象模拟类)
-     * 在JS中，函数是Function类的实例，Function间接继承自Object，所以函数也是一个对象。
+     * 声明一个全局变量用来注册命名空间
+     * @namespace Namespace
      */
-    var Base = {};
+    Namespace.register = function (namespace) {
+        var nsArr = namespace.split('.');
+        var nsStr = '', codeStr = '';
+        nsArr.forEach(function (ns, idx) {
+            nsStr += (idx === 0 ? ns : '.' + ns);
+            codeStr += 'if (typeof(' + nsStr + ') === "undefined") { ' + nsStr + ' = {}; }';
+        });
+        codeStr && eval(codeStr);
+    };
 
-    // 声明类的静态方法
+    factory(root.Namespace);
+
+})(window, function (Namespace) {
+
+    /**
+     * 注册[Base]命名空间
+     * @namespace Base
+     */
+    Namespace.register('Base');
+
     Base.format = function () {
         if (arguments.length == 0) {
-            return null;
+            return '';
         }
         var str = arguments[0];
         for (var i = 1; i < arguments.length; i++) {
             str = str.replace(new RegExp('\\{' + (i - 1) + '\\}', 'gm'), arguments[i]);
         }
         return str;
-    };
-
-    Base.formatString = function (string) {
-        var args = arguments;
-        var pattern = new RegExp('\\{([1-' + arguments.length + '])\\}', 'g');
-        return String(string).replace(pattern, function (match, index) {
-            return args[index];
-        });
     };
 
     Base.formatObject = function (str, obj) {
@@ -39,19 +49,8 @@
         });
     };
 
-    Base.generateRandomNumber = function () {
-        return new Date().getTime() * Math.floor(Math.random() * 1000000);
-    };
-
-    Base.generateGUID = function () {
-        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-            var r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
-            return v.toString(16);
-        });
-    };
-
-    if (typeof window === 'object' && typeof window.document === 'object') {
-        window.Base = Base;
+    Base.genRandomNum = function () {
+        return Base.format('{0}_{1}', new Date().getTime(), Math.floor(Math.random() * 1000000));
     }
 
-})(window);
+});

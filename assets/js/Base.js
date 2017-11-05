8fd01ac4ba35fa6b639bee0f070519c01f5cad1f
Gavin <lugavin@outlook.com>
2017-11-05 11:53:54 +0800

    Update

--- assets/js/Base.js
+++ assets/js/Base.js
@@ -1,29 +1,19 @@
+/*!
+ * Base.js v1.0
+ * https://lugavin.github.io/
+ * Copyright 2015-2017 Gavin Inc.
+ */
 (function (root, factory) {
 
-    if (typeof(Namespace) === 'undefined') {
-        Namespace = {};
+    if (typeof(root.Namespace) === 'undefined') {
+        throw new Error('Base\'s JavaScript requires Namespace v1.0 or later')
     }
-
-    /**
-     * 声明一个全局变量用来注册命名空间
-     * @namespace Namespace
-     */
-    Namespace.register = function (namespace) {
-        var nsArr = namespace.split('.');
-        var nsStr = '', codeStr = '';
-        nsArr.forEach(function (ns, idx) {
-            nsStr += (idx === 0 ? ns : '.' + ns);
-            codeStr += 'if (typeof(' + nsStr + ') === "undefined") { ' + nsStr + ' = {}; }';
-        });
-        codeStr && eval(codeStr);
-    };
 
     factory(root.Namespace);
 
 })(window, function (Namespace) {
 
     /**
-     * 注册[Base]命名空间
      * @namespace Base
      */
     Namespace.register('Base');
@@ -39,18 +29,12 @@
         return str;
     };
 
-    Base.formatObject = function (str, obj) {
-        /**
-         * \s 匹配任何空白字符
-         * /g 直接量语法(/regexp/g) 用于执行全局匹配
-         */
-        return str.replace(/\{\s*([^}\s]+)\s*}/g, function (match, p1, offset, string) {
-            return obj[p1]
-        });
-    };
-
     Base.genRandomNum = function () {
         return Base.format('{0}_{1}', new Date().getTime(), Math.floor(Math.random() * 1000000));
+    };
+
+    Base.isBoolean = function (value) {
+        return typeof value === 'boolean';
     };
 
     Base.isString = function (value) {
@@ -73,6 +57,29 @@
             return false;
         }
         return true;
+    };
+
+    Base.hasMobileUA = function () {
+        var nav = window.navigator;
+        var ua = nav.userAgent;
+        var pa = /iPad|iPhone|Android|Opera Mini|BlackBerry|webOS|UCWEB|Blazer|PSP|IEMobile|Symbian/g;
+        return pa.test(ua);
+    };
+
+    Base.isTablet = function () {
+        return window.screen.width < 992 && window.screen.width > 767 && Base.hasMobileUA();
+    };
+
+    Base.isMobile = function () {
+        return window.screen.width < 767 && Base.hasMobileUA();
+    };
+
+    Base.isDesktop = function () {
+        return !Base.isTablet() && !Base.isMobile();
+    };
+
+    Base.getURLParam = function (name) {
+        return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search) || [, ''])[1].replace(/\+/g, '%20')) || null;
     };
 
 });
--- assets/js/app.directives.js
+++
@@ -1,9 +0,0 @@
-(function () {
-
-    'use strict';
-
-    angular.module('app.directive', []);
-
-    angular.module('app.directive');
-
-})();

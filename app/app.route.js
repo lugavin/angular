e2d3f3f4c921a7b384e480a3876f970b3404f494
Gavin <lugavin@outlook.com>
2017-11-12 11:22:05 +0800

    Add angular-ui-router

--- app/app.route.js
+++ app/app.route.js
@@ -4,33 +4,62 @@
 
     angular
         .module('app.route', [
-            'ngRoute'
+            'ui.router'
         ]).config(config);
 
     /* @ngInject */
-    function config($routeProvider) {
-        $routeProvider
-            .when('/ui-bootstrap', {
-                templateUrl: 'app/bootstrap/index.html',
-                controller: 'BootstrapCtrl',
-                controllerAs: 'vm'
-            })
-            .when('/ui-grid', {
-                templateUrl: 'app/grid/index.html',
-                controller: 'GridCtrl',
-                controllerAs: 'vm'
-            })
-            .when('/ui-select', {
-                templateUrl: 'app/select/index.html',
-                controller: 'SelectCtrl',
-                controllerAs: 'vm'
-            })
-            .when('/welcome', {
-                templateUrl: 'app/welcome.html'
-            })
-            .otherwise({
-                redirectTo: '/welcome'
-            });
+    function config($stateProvider, $urlRouterProvider) {
+        $stateProvider.state('ui-bootstrap', {
+            url: '/ui-bootstrap',
+            templateUrl: 'app/bootstrap/index.html',
+            controller: 'BootstrapCtrl',
+            controllerAs: 'vm'
+        }).state('ui-grid', {
+            url: '/ui-grid',
+            templateUrl: 'app/grid/index.html',
+            controller: 'GridCtrl',
+            controllerAs: 'vm'
+        }).state('ui-select', {
+            url: '/ui-select',
+            templateUrl: 'app/select/index.html',
+            controller: 'SelectCtrl',
+            controllerAs: 'vm'
+        }).state('welcome', {
+            url: '/welcome',
+            templateUrl: 'app/welcome.html'
+        });
+        $urlRouterProvider.otherwise('welcome');
     }
+
+    // angular
+    //     .module('app.route', [
+    //         'ngRoute'
+    //     ]).config(config);
+    //
+    // /* @ngInject */
+    // function config($routeProvider) {
+    //     $routeProvider
+    //         .when('/ui-bootstrap', {
+    //             templateUrl: 'app/bootstrap/index.html',
+    //             controller: 'BootstrapCtrl',
+    //             controllerAs: 'vm'
+    //         })
+    //         .when('/ui-grid', {
+    //             templateUrl: 'app/grid/index.html',
+    //             controller: 'GridCtrl',
+    //             controllerAs: 'vm'
+    //         })
+    //         .when('/ui-select', {
+    //             templateUrl: 'app/select/index.html',
+    //             controller: 'SelectCtrl',
+    //             controllerAs: 'vm'
+    //         })
+    //         .when('/welcome', {
+    //             templateUrl: 'app/welcome.html'
+    //         })
+    //         .otherwise({
+    //             redirectTo: '/welcome'
+    //         });
+    // }
 
 })();
